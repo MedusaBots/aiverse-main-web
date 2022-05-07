b5b0322df68ac4fc6b1992f0204c5a900d297eb6
devulapallisai <ee20btech11013@iith.ac.in>
2022-05-07 18:25:59 +0530

    added vision part

---
+++ saiprachodhan/src/components/Fifthslide.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../images/logo.png";
+function Fifthslide() {
+  return (
+    <div className="md:mx-[18vw] mx-[8vw] w-[84vw] md:w-[64vw] overflow-hidden">
+      <h1 className="text-center md:text-left font-headingbold font-bold text-6xl text-white">
+        Our Vision
+      </h1>
+      <div className="flex md:flex-row mx-auto flex-col-reverse w-[100%] md:w-[100%] py-6">
+        <div className="w-[100%] md:w-[50%] my-auto">
+          <div className="rounded-3xl border-2 border-[#B5B5B5] max-w-[90%] p-6 text-[#E9E9E9] font-mont text-[12px] leading-[18px] mx-auto">
+            We wish to create a platform where the users can get the access to
+            various ai based applications helping over all development and
+            growth in their professional life and free up their burden, time and
+            resources. It can help startups to scale, give better satisfaction
+            to customers, automate repetitive tasks and improve the quality of
+            work. At an individual level, these services can help its users to
+            overcome their boredom, relieve stress and perform creative tasks in
+            their free time
+          </div>
+        </div>
+        <div className="w-[100%] mb-8 mx-auto md:mb-0 md:w-[50%]">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[100%] bg-[linear-gradient(200.55deg, #163845 -4.6%, #1C3851 48.38%, #162642 102.49%)]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Fifthslide;
--- saiprachodhan/src/components/Thirdslide.tsx
+++ saiprachodhan/src/components/Thirdslide.tsx
@@ -5,6 +5,7 @@ import "aos/dist/aos.css";
 import Dots from "../images/Dots.png";
 import Ball from "../images/ball.png";
 import Fourthslide from "./Fourthslide";
+import Fifthslide from "./Fifthslide";
 
 function Thirdslide() {
   useEffect(() => {
@@ -12,131 +13,137 @@ function Thirdslide() {
     AOS.refresh();
   }, []);
   return (
-    <div className={`${styles.background}`}>
-      <div className="absolute left-[2vw] top-[230vh] z-[8] md:hidden block">
-        <img src={Dots} alt="ima" className="w-[80vw]" />
-      </div>
-      <div className="absolute right-0 top-[230vh] z-[10] md:hidden block">
-        <img src={Ball} alt="ima" className="w-[80vw] opacity-[0.5]" />
-      </div>
-      <div className="nw:ml-[8vw]">
-        <h1 className="text-center md:text-left pt-[5vh] font-headingbold font-bold text-6xl text-white py-2">
-          Pillers Of aiverse
-        </h1>
-        <br />
-        <br />
-        <br />
-        <div className="cards nw:mr-[8vw] md:flex-row flex-col flex justify-between">
-          <div
-            className="z-[10000000000] card my-[20px] min-h-[350px] m-auto md:mx-4 w-[240px] relative rounded-lg"
-            data-aos="fade-up"
-            data-aos-duration="1500"
-            style={{
-              background: "#122C48",
-              border: "2px solid rgba(255, 255, 255, 0.05)",
-              backdropFilter: "blur(41px)",
-              boxShadow: "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-            }}
-          >
-            <div
-              className="transition-all duration-1000 ease-in-out absolute hover:top-[-20px] hover:left-[-20px] min-h-[350px] w-[240px]  rounded-lg z-[10000000000]"
-              style={{
-                background:
-                  "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
-                boxShadow:
-                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-                backdropFilter: "blur(20.3526px)",
-              }}
-            >
-              <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
-                Aiverse characters have emerged from a metaverse who will share
-                their powers to users. Each characters of aiverse have specific
-                power which can be leveraged by users in the form of AI
-                applications
-              </div>
-              <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
-                Aiverse characters have emerged from a metaverse who will share
-                their powers to users. Each characters of aiverse have specific
-                power which can be leveraged by users in the form of AI
-                applications
-              </div>
-            </div>
-          </div>
-          <div
-            className="card min-h-[350px] my-[20px] m-auto md:mx-4 w-[240px] relative rounded-lg  z-[10000000000]"
-            style={{
-              border: "2px solid rgba(255, 255, 255, 0.05)",
-              backdropFilter: "blur(41px)",
-              background: "#122C48",
-              boxShadow: "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-            }}
-            data-aos="fade-up"
-            data-aos-duration="1500"
-          >
-            <div
-              className="transition-all duration-1000 ease-in-out absolute hover:top-[20px] hover:left-[20px] min-h-[350px] w-[240px] z-[10] rounded-lg"
-              style={{
-                background:
-                  "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
-                boxShadow:
-                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-                backdropFilter: "blur(20.3526px)",
-              }}
-            >
-              <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
-                Use-2-earn- Users can earn AIV tokens as they use our AI models
-                . Higher the no. Of requests per day more aiv tokens will be
-                awarded to users at the end of the day
-              </div>
-              <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
-                Use-2-earn- Users can earn AIV tokens as they use our AI models
-                . Higher the no. Of requests per day more aiv tokens will be
-                awarded to users at the end of the day
-              </div>
-            </div>
-          </div>
-          <div
-            className="card my-[20px] min-h-[350px] m-auto md:mx-4 w-[240px] relative rounded-lg z-[10000000000]"
-            style={{
-              border: "2px solid rgba(255, 255, 255, 0.05)",
-              backdropFilter: "blur(41px)",
-              background: "#122C48",
-              boxShadow: "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-            }}
-            data-aos="fade-up"
-            data-aos-duration="1500"
-          >
-            <div
-              className="transition-all duration-1000 ease-in-out absolute hover:top-[-20px] hover:left-[20px] min-h-[350px] w-[240px] z-[10] rounded-lg"
-              style={{
-                boxShadow:
-                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
-                background:
-                  "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
-                backdropFilter: "blur(20.3526px)",
-              }}
-            >
-              <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
-                Aiverse will be providing decentralized data set pools where
-                developers can upload their data sets/ predictions and these can
-                be access by consumers. Ai verse will also be providing its
-                ready to use data sets
-              </div>
-              <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
-                Aiverse will be providing decentralized data set pools where
-                developers can upload their data sets/ predictions and these can
-                be access by consumers. Ai verse will also be providing its
-                ready to use data sets
-              </div>
-            </div>
-          </div>
+    <>
+      <div className={`${styles.background}`}>
+        <div className="absolute left-[2vw] top-[230vh] z-[8] md:hidden block">
+          <img src={Dots} alt="ima" className="w-[80vw]" />
         </div>
-        <br />
-        <br />
-        <br />
-        <Fourthslide />
+        <div className="absolute right-0 top-[230vh] z-[10] md:hidden block">
+          <img src={Ball} alt="ima" className="w-[80vw] opacity-[0.5]" />
+        </div>
+        <div className="nw:ml-[8vw]">
+          <h1 className="text-center md:text-left pt-[5vh] font-headingbold font-bold text-6xl text-white py-2">
+            Pillers Of aiverse
+          </h1>
+          <br />
+          <br />
+          <br />
+          <div className="cards nw:mr-[8vw] md:flex-row flex-col flex justify-between">
+            <div
+              className="z-[10000000000] card my-[20px] min-h-[350px] m-auto md:mx-4 w-[240px] relative rounded-lg"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              style={{
+                background: "#122C48",
+                border: "2px solid rgba(255, 255, 255, 0.05)",
+                backdropFilter: "blur(41px)",
+                boxShadow:
+                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+              }}
+            >
+              <div
+                className="transition-all duration-1000 ease-in-out absolute hover:top-[-20px] hover:left-[-20px] min-h-[350px] w-[240px]  rounded-lg z-[10000000000]"
+                style={{
+                  background:
+                    "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
+                  boxShadow:
+                    "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+                  backdropFilter: "blur(20.3526px)",
+                }}
+              >
+                <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
+                  Aiverse characters have emerged from a metaverse who will
+                  share their powers to users. Each characters of aiverse have
+                  specific power which can be leveraged by users in the form of
+                  AI applications
+                </div>
+                <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
+                  Aiverse characters have emerged from a metaverse who will
+                  share their powers to users. Each characters of aiverse have
+                  specific power which can be leveraged by users in the form of
+                  AI applications
+                </div>
+              </div>
+            </div>
+            <div
+              className="card min-h-[350px] my-[20px] m-auto md:mx-4 w-[240px] relative rounded-lg  z-[10000000000]"
+              style={{
+                border: "2px solid rgba(255, 255, 255, 0.05)",
+                backdropFilter: "blur(41px)",
+                background: "#122C48",
+                boxShadow:
+                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+              }}
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            >
+              <div
+                className="transition-all duration-1000 ease-in-out absolute hover:top-[20px] hover:left-[20px] min-h-[350px] w-[240px] z-[10] rounded-lg"
+                style={{
+                  background:
+                    "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
+                  boxShadow:
+                    "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+                  backdropFilter: "blur(20.3526px)",
+                }}
+              >
+                <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
+                  Use-2-earn- Users can earn AIV tokens as they use our AI
+                  models . Higher the no. Of requests per day more aiv tokens
+                  will be awarded to users at the end of the day
+                </div>
+                <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
+                  Use-2-earn- Users can earn AIV tokens as they use our AI
+                  models . Higher the no. Of requests per day more aiv tokens
+                  will be awarded to users at the end of the day
+                </div>
+              </div>
+            </div>
+            <div
+              className="card my-[20px] min-h-[350px] m-auto md:mx-4 w-[240px] relative rounded-lg z-[10000000000]"
+              style={{
+                border: "2px solid rgba(255, 255, 255, 0.05)",
+                backdropFilter: "blur(41px)",
+                background: "#122C48",
+                boxShadow:
+                  "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+              }}
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            >
+              <div
+                className="transition-all duration-1000 ease-in-out absolute hover:top-[-20px] hover:left-[20px] min-h-[350px] w-[240px] z-[10] rounded-lg"
+                style={{
+                  boxShadow:
+                    "-23.609px 48.8461px 73.2692px rgba(23, 18, 43, 0.55)",
+                  background:
+                    "linear-gradient(210.96deg, #091E35 0.01%, #142D4D 42.05%, #0B273C 104.81%)",
+                  backdropFilter: "blur(20.3526px)",
+                }}
+              >
+                <div className="text-white font-poppins mx-auto text-center text-[14px] invisible p-2">
+                  Aiverse will be providing decentralized data set pools where
+                  developers can upload their data sets/ predictions and these
+                  can be access by consumers. Ai verse will also be providing
+                  its ready to use data sets
+                </div>
+                <div className="text-white font-poppins  mx-auto text-center text-[12px] p-2 font-extralight tracking-[0.02em]">
+                  Aiverse will be providing decentralized data set pools where
+                  developers can upload their data sets/ predictions and these
+                  can be access by consumers. Ai verse will also be providing
+                  its ready to use data sets
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <Fourthslide />
+        </div>
       </div>
-    </div>
+      <Fifthslide />
+    </>
   );
 }
 
