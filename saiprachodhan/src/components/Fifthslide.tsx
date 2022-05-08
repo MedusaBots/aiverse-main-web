7e95858aa5c9926c37ffe1806652bb5d8d0b16a9
devulapallisai <ee20btech11013@iith.ac.in>
2022-05-09 02:20:12 +0530

    added

--- saiprachodhan/src/components/Fifthslide.tsx
+++ saiprachodhan/src/components/Fifthslide.tsx
@@ -44,9 +44,10 @@ function Fifthslide() {
               }
             >
               <h3 className="text-3xl font-bold py-4">2022</h3>
-              <li>Development of two AIs</li>
-              <li>Get backers and raise funds</li>
-              <li>Launch of NFTs</li>
+              <li>Development of website</li>
+              <li>Start Partnerships & Getting Backers</li>
+              <li>Development of Whitepaper</li>
+              <li>Development of Meditor</li>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -78,11 +79,9 @@ function Fifthslide() {
               }
             >
               <h3 className="text-3xl font-bold py-4">2022</h3>
-              <li>Launch of AIV coins</li>
-              <li>Release of chat bot ai</li>
-              <li>
-                Development of decentralized pool of dataset & prediction models
-              </li>
+              <li>Development of Generative AI</li>
+              <li>launch of Generative AI NFTs</li>
+              <li>Start Development of Marketplace</li>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -114,9 +113,10 @@ function Fifthslide() {
               }
             >
               <h3 className="text-3xl font-bold py-4">2022</h3>
-              <li>Development Of Spam detector ai</li>
-              <li>Development of Data analytic ai</li>
-              <li>Launch of data analytic and spam detector ai </li>
+              <li>Launch of aiver tokens</li>
+              <li>Start Early Access of Marketplace</li>
+              <li>Development & Launch of NLP, NLG AI on Marketplace </li>
+              <li>Conduct Hackathons, Events </li>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -148,9 +148,11 @@ function Fifthslide() {
               }
             >
               <h3 className="text-3xl font-bold py-4">2022</h3>
-              <li>Start development of photo editor and ar ai </li>
-              <li>Release of photo editor and ar ai</li>
-              <li>Start development of anti virus and cyber security ai</li>
+              <li>Development of Data Analytic AI </li>
+              <li>
+                Development of decentralized pool of dataset & prediction models{" "}
+              </li>
+              <li>Start of development of Game AI</li>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -182,9 +184,78 @@ function Fifthslide() {
               }
             >
               <h3 className="text-3xl font-bold py-4">2023</h3>
-              <li>Release of antivirus and cyber security ai </li>
-              <li>Start of development of game ai </li>
-              <li>Launch of game ai </li>
+              <li>Start development of Photo Editor AI </li>
+              <li>Development and Release of Spam detector ai</li>
+              <li>Release of Data Analytics AI</li>
+              <li>Launch of Marketplace</li>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "#fff", color: "#000" }}
+              iconStyle={{
+                background: "#00D578",
+                color: "#fff",
+                borderRadius: "12px",
+                transform: "rotate(45deg)",
+              }}
+              // contentArrowStyle={{ borderRight: "7px solid  #00D578" }}
+              icon={
+                <>
+                  <div
+                    style={{
+                      transform: "rotate(-45deg)",
+                    }}
+                    className="text-black text-center 
+                text-xl font-semibold
+                leading-[40px] h-[40px] w-[40px]
+                roadmap:leading-[58px] roadmap:h-[58px] roadmap:w-[58px]"
+                    // the roadmap resizes at 1170px
+                  >
+                    <p style={{}} className="">
+                      Q4
+                    </p>
+                  </div>
+                </>
+              }
+            >
+              <h3 className="text-3xl font-bold py-4">2023</h3>
+              <li>Start dvelopment of anti virus and cyber security ai</li>
+              <li>Release of Photo Editor AI</li>
+              <li>Partnerships with Artists, Digital Artists, etc.</li>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "#fff", color: "#000" }}
+              iconStyle={{
+                background: "#00D578",
+                color: "#fff",
+                borderRadius: "12px",
+                transform: "rotate(45deg)",
+              }}
+              // contentArrowStyle={{ borderRight: "7px solid  #00D578" }}
+              icon={
+                <>
+                  <div
+                    style={{
+                      transform: "rotate(-45deg)",
+                    }}
+                    className="text-black text-center 
+                text-xl font-semibold
+                leading-[40px] h-[40px] w-[40px]
+                roadmap:leading-[58px] roadmap:h-[58px] roadmap:w-[58px]"
+                    // the roadmap resizes at 1170px
+                  >
+                    <p style={{}} className="">
+                      Q1
+                    </p>
+                  </div>
+                </>
+              }
+            >
+              <h3 className="text-3xl font-bold py-4">2023</h3>
+              <li>Release of antivirus and cyber security ai</li>
+              <li>Launch of game ai</li>
+              <li>Launch of Meditor</li>
             </VerticalTimelineElement>
           </VerticalTimeline>
         </div>
