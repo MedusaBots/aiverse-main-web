590b4c25dcc2d2a32e3f24656b1b800a9fabbcb2
devulapallisai <ee20btech11013@iith.ac.in>
2022-05-11 11:07:54 +0530

    added

--- saiprachodhan/src/components/Firstslide.tsx
+++ saiprachodhan/src/components/Firstslide.tsx
@@ -4,6 +4,11 @@ import menu from "../images/menu.png";
 import cross from "../images/cross.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import discover from "../images/discover.png";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import { Bitski } from "bitski";
 import Logo2 from "../images/logo2.png";
 import {
   faInstagram,
@@ -19,34 +24,50 @@ function Firstslide() {
 
   // Requests access to the user's META MASK WALLET
   // https://metamask.io
-  async function requestAccount() {
-    console.log("Requesting account...");
+  const providerOptions = {
+    bitski: {
+      package: Bitski, // required
+      options: {
+        clientId: "BITSKI_CLIENT_ID", // required
+        callbackUrl: "BITSKI_CALLBACK_URL", // required
+      },
+    },
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        infuraId: "INFURA_ID", // required
+      },
+    },
+    coinbasewallet: {
+      package: CoinbaseWalletSDK, // Required
+      options: {
+        appName: "web3modal", // Required
+        infuraId: "INFURA_ID", // Required
+        rpc: "", // Optional if `infuraId` is provided; otherwise it's required
+        chainId: 1, // Optional. It defaults to 1 if not provided
+        darkMode: false, // Optional. Use dark theme, defaults to false
+      },
+    },
+    binancechainwallet: {
+      package: true,
+    },
+  };
 
-    // ❌ Check if Meta Mask Extension exists
-    if (window.ethereum) {
-      console.log("detected");
+  const web3Modal = new Web3Modal({
+    network: "mainnet", // optional
+    cacheProvider: true, // optional
+    disableInjectedProvider: true,
+    providerOptions, // required
+  });
 
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log("Error connecting...");
-      }
-    } else {
-      alert("Meta Mask not detected");
-    }
-  }
-
-  // Create a provider to interact with a smart contract
-  async function connectWallet() {
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-    }
-  }
+  const connectWallet = async () => {
+    const provider = await web3Modal.connect();
+    const web3 = new Web3(provider);
+    await window.ethereum.send("eth_requestAccounts");
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    setWalletAddress(account);
+  };
   useEffect(() => {
     if (walletAddress != "") {
       connectWallet();
