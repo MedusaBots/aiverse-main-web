fdafff4bdbcf6ca5d9e2135a6a69af358f44c057
devulapallisai <ee20btech11013@iith.ac.in>
2022-05-12 14:25:58 +0530

    added

--- saiprachodhan/src/components/Tokenomics1.tsx
+++ saiprachodhan/src/components/Tokenomics1.tsx
@@ -21,40 +21,10 @@ import Logo2 from "../images/logo2.png";
 import Menu from "./Menu";
 
 function Tokenomics1() {
-  const [walletAddress, setWalletAddress] = useState("");
-
   // Helper Functions
 
   // Requests access to the user's META MASK WALLET
   // https://metamask.io
-  async function requestAccount() {
-    console.log("Requesting account...");
-
-    // ❌ Check if Meta Mask Extension exists
-    if (window.ethereum) {
-      console.log("detected");
-
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log("Error connecting...");
-      }
-    } else {
-      alert("Meta Mask not detected");
-    }
-  }
-
-  // Create a provider to interact with a smart contract
-  async function connectWallet() {
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-    }
-  }
   const [dropdown, setdropdown] = useState(false);
   return (
     <>
